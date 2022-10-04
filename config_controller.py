767c4f7a7061eb2521b3f1548fe7fc881e295040
koshou <koshou@singularwings.com>
2022-10-04 09:03:14 +0800

    * version = 0.1.5     - fix:         - fixed develop config         - fixed version number

--- config_controller.py
+++ config_controller.py
@@ -36,16 +36,10 @@ class ConfigDevelop(ConfigMaster):
         self.db_name = 'report_server_develop'
         with open('mysql_config.json') as f:
             content = json.load(f)
-<<<<<<< HEAD
-        self.engine_url = 'mysql+pymysql://{}:{}@{}:{}/{}'.format(content[self.mode_name]['name'], content[self.mode_name]['password'], 'localhost', content[self.mode_name]['post'], content[self.mode_name]['schema'])
-
-
-=======
-        self.mysql_engine_url = 'mysql+pymysql://{}:{}@{}:{}/{}'.format(content[self.mode_name]['name'], content[self.mode_name]['password'], content[self.mode_name]['host'], content[self.mode_name]['post'], content[self.mode_name]['algorithm_db'])
+        self.mysql_engine_url = 'mysql+pymysql://{}:{}@{}:{}/{}'.format(content[self.mode_name]['name'], content[self.mode_name]['password'], 'localhost', content[self.mode_name]['post'], content[self.mode_name]['algorithm_db'])
         with open('mongo_config.json') as f:
             content = json.load(f)
-        self.mongodb_engine_url = 'mongodb://{}:{}@{}:{}/?authMechanism=DEFAULT'.format(content[self.mode_name]['name'], content[self.mode_name]['password'], content[self.mode_name]['host'], content[self.mode_name]['post'])
->>>>>>> 87c31eb4e9f939a466736dfe11ed92e444b13083
+        self.mongodb_engine_url = 'mongodb://{}:{}@{}:{}/?authMechanism=DEFAULT'.format(content[self.mode_name]['name'], content[self.mode_name]['password'], 'localhost', content[self.mode_name]['post'])
 
 
 class ConfigRelease(ConfigDevelop):
